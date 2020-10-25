7f933c3eeeb3406ee39d8be4258d5a56dea57ecb
Kadir Islow <kadirislow@gmail.com>
2020-10-25 21:37:51 +0100

    add index.html, styles and js

---
+++ public/js/diff.js
@@ -0,0 +1,77 @@
+import './main.js';
+
+'use strict';
+(function () {
+  const diff = require('diff');
+  const ACTIVE_CLASS = 'active';
+  let $left = document.querySelector('#left');
+  let $right = document.querySelector('#right');
+  let $form = document.querySelector('#diff-form');
+  let $insert = document.querySelector('#diff-insert');
+  let $result = document.querySelector('#diff-result');
+  let $resultContainer = document.querySelector('#diff-result > section');
+  let $backBtn = document.querySelector('.diff-result__btn-back');
+  let isInputView = true;
+
+  $form.addEventListener('submit', onSubmit);
+
+  $backBtn.addEventListener('click', onBack);
+
+  /**
+   * On diff form submit
+   */
+  function onSubmit(e) {
+    e.preventDefault();
+
+    let changes = diff.diffChars($left.value, $right.value);
+    let result = '';
+    $resultContainer.innerHTML = '';
+
+    changes.forEach(change => {
+      const text = escapeHTML(change.value);
+      let className = null;
+
+      if (change.removed) {
+        className = 'removed';
+      } else if (change.added) {
+        className = 'added';
+      }
+
+      result += wrapText(text, className);
+    });
+
+    $resultContainer.innerHTML += result;
+
+    toggleView();
+  }
+
+  function wrapText(value, className = null) {
+    return `<span class="${ className || '' }">${ value }</span>`;
+  }
+
+  function toggleView() {
+    isInputView = !isInputView;
+
+    if (isInputView) {
+      $result.classList.remove(ACTIVE_CLASS);
+      $insert.classList.add(ACTIVE_CLASS);
+    } else {
+      $result.classList.add(ACTIVE_CLASS);
+      $insert.classList.remove(ACTIVE_CLASS);
+    }
+  }
+
+  function onBack() {
+    toggleView();
+  }
+
+  function escapeHTML(string) {
+    return string
+      .replace(new RegExp('&', 'g'), '&amp;')
+      .replace(new RegExp('<', 'g'), '&lt;')
+      .replace(new RegExp('>', 'g'), '&gt;')
+      .replace(new RegExp('"', 'g'), '&quot;')
+      .replace(new RegExp(' ', 'g'), '&nbsp;')
+      .replace(new RegExp('\n', 'g'), '<br>');
+  }
+})();
---
+++ public/js/main.js
@@ -0,0 +1,206 @@
+const { app, BrowserWindow, Menu } = require('electron');
+const path = require('path');
+const url = require('url');
+
+let win;
+
+function createWindow () {
+  win = new BrowserWindow({
+    width: 800,
+    height: 600,
+    titleBarStyle: 'hidden',
+    minHeight: 150,
+    minWidth: 380,
+    title: 'Diffusive'
+  });
+
+  win.loadURL(url.format({
+    pathname: path.join(__dirname, 'public/js', 'diffirenza.html'),
+    protocol: 'file:',
+    slashes: true
+  }));
+
+  win.on('closed', () => {
+    win = null
+  })
+}
+
+
+function createMenu() {
+  const template = [
+    {
+      label: 'Edit',
+      submenu: [
+        {
+          role: 'undo'
+        },
+        {
+          role: 'redo'
+        },
+        {
+          type: 'separator'
+        },
+        {
+          role: 'cut'
+        },
+        {
+          role: 'copy'
+        },
+        {
+          role: 'paste'
+        },
+        {
+          role: 'delete'
+        },
+        {
+          role: 'selectall'
+        }
+      ]
+    },
+    {
+      label: 'View',
+      submenu: [
+        {
+          role: 'reload'
+        },
+        {
+          role: 'forcereload'
+        },
+        {
+          role: 'toggledevtools'
+        },
+        {
+          type: 'separator'
+        },
+        {
+          role: 'togglefullscreen'
+        }
+      ]
+    },
+    {
+      role: 'window',
+      submenu: [
+        {
+          role: 'minimize'
+        },
+        {
+          role: 'close'
+        }
+      ]
+    },
+    {
+      role: 'help',
+      submenu: [
+        {
+          label: 'Learn More',
+          click () { require('electron').shell.openExternal('https://appheim.github.io/diffusive/') }
+        },
+        {
+          label: 'Report issue',
+          click () { require('electron').shell.openExternal('https://github.com/appheim/diffusive/issues') }
+        }
+      ]
+    }
+  ];
+
+  if (process.platform === 'darwin') {
+    template.unshift({
+      label: app.getName(),
+      submenu: [
+        {
+          role: 'about'
+        },
+        {
+          type: 'separator'
+        },
+        {
+          role: 'services',
+          submenu: []
+        },
+        {
+          type: 'separator'
+        },
+        {
+          role: 'hide'
+        },
+        {
+          role: 'hideothers'
+        },
+        {
+          role: 'unhide'
+        },
+        {
+          type: 'separator'
+        },
+        {
+          role: 'quit'
+        }
+      ]
+    });
+    // Edit menu.
+    template[1].submenu.push(
+      {
+        type: 'separator'
+      },
+      {
+        label: 'Speech',
+        submenu: [
+          {
+            role: 'startspeaking'
+          },
+          {
+            role: 'stopspeaking'
+          }
+        ]
+      }
+    );
+    // Window menu.
+    template[3].submenu = [
+      {
+        label: 'Close',
+        accelerator: 'CmdOrCtrl+W',
+        role: 'close'
+      },
+      {
+        label: 'Minimize',
+        accelerator: 'CmdOrCtrl+M',
+        role: 'minimize'
+      },
+      {
+        label: 'Zoom',
+        role: 'zoom'
+      },
+      {
+        type: 'separator'
+      },
+      {
+        label: 'Bring All to Front',
+        role: 'front'
+      }
+    ];
+  }
+
+  app.on('ready', () => {
+    createMenu();
+    createWindow();
+  });
+  
+  app.on('window-all-closed', () => {
+    // On macOS it is common for applications and their menu bar
+    // to stay active until the user quits explicitly with Cmd + Q
+    if (process.platform !== 'darwin') {
+      app.quit();
+    }
+  });
+  
+  app.on('activate', () => {
+    // On macOS it's common to re-create a window in the app when the
+    // dock icon is clicked and there are no other windows open.
+    if (win === null) {
+      createWindow();
+    }
+  });
+
+  const menu = Menu.buildFromTemplate(template);
+  Menu.setApplicationMenu(menu);
+}
